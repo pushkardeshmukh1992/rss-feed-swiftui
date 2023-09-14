49c3ce02f8f420acb8efee3e87798ea311217dc2
Admin <admin@Admins-MacBook-Pro.local>
2023-09-14 16:35:24 +0530

    init commit

---
+++ RSSFeed/RSSFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSSFeedApp.swift
+//  RSSFeed
+//
+//  Created by Admin on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RSSFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
