5326281d49cff945625e309162f3e4f6628b6169
Pushkar Deshmukh <pushkar2192@gmail.com>
2023-09-17 01:24:53 +0530

    Add feedservice tests

--- RSSFeedTests/NetworkingManager/FeedServiceTests.swift
+++ RSSFeedTests/NetworkingManager/FeedServiceTests.swift
@@ -11,7 +11,6 @@ import XCTest
 class FeedServiceTests: XCTestCase {
     func test_deliversSuccessResponseOnValidURL() throws {
         let url = DataUtil.publication1.url
-        let session = getSession()
         
         guard let path = Bundle.main.path(forResource: "Example", ofType: "xml"),
               let data = FileManager.default.contents(atPath: path) else {
@@ -20,13 +19,8 @@ class FeedServiceTests: XCTestCase {
         }
         
         let feed = try XMLDecoderUtil.getXMLDecoder().decode(RSS.self, from: data.dataMiddleware())
-    
         
-        MockURLSessionProtocol.loadingHandler = {
-            let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
-            return (response!, data, nil)
-        }
-        
+        let session = getSession(data: data, error: nil)
         let sut = FeedService()
         sut.getFeed(url: url, session: session) { result in
             XCTAssertEqual(result, .success(feed))
@@ -35,13 +29,7 @@ class FeedServiceTests: XCTestCase {
     
     func test_deliversFailureResponseOnInvalidURL() {
         let url = DataUtil.publication1.url
-        let session = getSession()
-        
-        MockURLSessionProtocol.loadingHandler = {
-            
-            let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
-            return (response!, nil, APIError.httpError)
-        }
+        let session = getSession(data: nil, error: .httpError)
         
         let exp = expectation(description: "Feed service fetch should return on network failure")
         
@@ -61,12 +49,7 @@ class FeedServiceTests: XCTestCase {
     
     func test_deliversFailureResponseOnInvalidData() {
         let url = DataUtil.publication1.url
-        let session = getSession()
-
-        MockURLSessionProtocol.loadingHandler = {
-            let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
-            return (response!, FeedServiceTests.getInvalidData(), nil)
-        }
+        let session = getSession(data: FeedServiceTests.getInvalidData(), error: nil)
 
         let exp = expectation(description: "Feed service fetch should return error on invalid data response")
 
@@ -77,7 +60,28 @@ class FeedServiceTests: XCTestCase {
                 XCTFail("Feed service returned success instead of failure")
             case .failure(let error):
                 print(error)
-//                XCTAssertEqual(error, APIError.dataError)
+                XCTAssertEqual(error, APIError.parseError)
+                exp.fulfill()
+            }
+        }
+
+        wait(for: [exp], timeout: 3)
+    }
+    
+    func test_deliversFailureResponseOnEmptyData() {
+        let url = DataUtil.publication1.url
+        let session = getSession(data: nil, error: nil)
+
+        let exp = expectation(description: "Feed service fetch should return error on invalid data response")
+
+        let sut = FeedService()
+        sut.getFeed(url: url, session: session) { result in
+            switch result {
+            case .success(_):
+                XCTFail("Feed service returned success instead of failure")
+            case .failure(let error):
+                print(error)
+                XCTAssertEqual(error, APIError.dataError)
                 exp.fulfill()
             }
         }
@@ -92,9 +96,50 @@ class FeedServiceTests: XCTestCase {
         return URLSession(configuration: configuration)
     }
     
+    private func getSession(data: Data?, error: APIError?) -> URLSession {
+        let session = URLSessionMock()
+        session.data = data
+        session.error = error
+        return session
+    }
+    
     static func getInvalidData() -> Data {
         let json = ["items": ""]
         return try! JSONSerialization.data(withJSONObject: json)
     }
 
+}
+
+
+class URLSessionDataTaskMock: URLSessionDataTask {
+    private let closure: () -> Void
+
+    init(closure: @escaping () -> Void) {
+        self.closure = closure
+    }
+
+    // We override the 'resume' method and simply call our closure
+    // instead of actually resuming any task.
+    override func resume() {
+        closure()
+    }
+}
+
+class URLSessionMock: URLSession {
+    typealias CompletionHandler = (Data?, URLResponse?, Error?) -> Void
+
+    var data: Data?
+    var error: Error?
+    
+    override func dataTask(
+        with url: URL,
+        completionHandler: @escaping CompletionHandler
+    ) -> URLSessionDataTask {
+        let data = self.data
+        let error = self.error
+
+        return URLSessionDataTaskMock {
+            completionHandler(data, nil, error)
+        }
+    }
 }
