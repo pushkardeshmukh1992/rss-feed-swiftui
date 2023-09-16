84fa351922211fec65d491c611a7420b04cfbe5d
Pushkar Deshmukh <pushkar2192@gmail.com>
2023-09-16 12:43:08 +0530

    Add test to check publication toggle

--- RSSFeedTests/RSSFeedTests.swift
+++ RSSFeedTests/RSSFeedTests.swift
@@ -13,5 +13,25 @@ final class RSSFeedTests: XCTestCase {
     func test() {
         XCTAssertEqual("test", "test")
     }
+    
+    // MARK: Publication view model tests
+    func testPublications_initialLoadShouldHaveAtLeastOneActivePublication() {
+        let sut = PublicationsViewModel()
+        
+        XCTAssertNotEqual(sut.publications.count, 0)
+        XCTAssertEqual(sut.publications[0].active, true)
+    }
+    
+    func testPublications_publicationShouldToggleActiveFlag() {
+        let sut = PublicationsViewModel()
+        
+        let oldPublicationState = sut.publications[0]
+        
+        sut.toggleActivePublication(publication: oldPublicationState)
+        
+        let newPublicationState = sut.publications[0]
+        
+        XCTAssertNotEqual(oldPublicationState.active, newPublicationState.active)
+    }
 
 }
