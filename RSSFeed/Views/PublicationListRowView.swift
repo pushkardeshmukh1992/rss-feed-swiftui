65924e72c07ce24309992cf9cab0119e1ed04870
Pushkar Deshmukh <pushkar2192@gmail.com>
2023-09-17 19:18:28 +0530

    Update background color so thats its visible on dark theme

--- RSSFeed/Views/PublicationListRowView.swift
+++ RSSFeed/Views/PublicationListRowView.swift
@@ -16,6 +16,7 @@ struct PublicationListRowView: View {
                 .resizable()
                 .scaledToFit()
                 .padding(0)
+                .background(.white.opacity(1))
         }
     }
 }
